e2e5e5d4a3e5f6ee12cc5acdf360ce0497d1f261
Derek Tracy <tracyde@gmail.com>
2015-03-05 23:14:41 +0000

    rake spec finally runs

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 describe 'stigs' do
 
   context 'with defaults for all parameters' do
+    let(:facts) {{ :kernel => 'Linux', 
+	    :concat_basedir => '/tmp/concat',
+	    :operatingsystem => 'RedHat',
+	    :osfamily => 'RedHat',
+	    :operatingsystemrelease => 6.6,
+            :operatingsystemmajrelease => 6 }}
     it { should contain_class('stigs') }
   end
 end
